b345770d19202d343f56c33086bbbe7a1dff1c2d
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-08 20:21:13 -0300

    Teste de pipeline do heroku

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const SearchController = require('./controllers/SearchController');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  return res.send("Hello from <a href='https://github.com/LeandrodaSilva/dictionary-express'>Dictionary API</a>")
+  return res.send("Hello from <a href='https://github.com/LeandrodaSilva/dictionary-express'>Dictionary API</a>.")
 });
 routes.get('/search', SearchController.create);
 
