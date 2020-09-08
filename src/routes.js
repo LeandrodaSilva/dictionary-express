f1199b8b100cf46b654b8feaeb5828e2236fd14b
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-08 20:05:01 -0300

    Adicionado rota principal

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const SearchController = require('./controllers/SearchController');
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  return res.send("Hello from <a href='https://github.com/LeandrodaSilva/dictionary-express'>Dictionary API</a>")
+});
 routes.get('/search', SearchController.create);
 
 module.exports = routes;
