975bb4907c92fef949a2adccee93e15576d8a0ed
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-11-03 21:12:13 -0300

    change database

--- knexfile.js
+++ knexfile.js
@@ -44,19 +44,17 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './src/database/db.sqlite'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    },
+    useNullAsDefault: true,
   }
 
 };
